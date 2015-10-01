2c5e737c5cbf07310390107f2f28b6f2b99a01f7
dgvncsz0f <dsouza@c0d3.xxx>
2015-10-01 14:47:26 -0300

    bugfix: wrong haproxy port

--- etc/haproxy-svc-template.sh
+++ etc/haproxy-svc-template.sh
@@ -8,7 +8,7 @@ cat <<EOF
         "tags": ["pong"],
         "port": 8000,
         "check": {
-            "script": "nc-check -w5 -zv localhost 8000",
+            "script": "nc-check -w5 -zv localhost 4500",
             "interval": "5s"
         }
     }
